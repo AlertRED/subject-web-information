6447abd04cf9ef7abec3f0c9965f64f70b7e7d43
Alexandr <ieaiaio@bk.ru>
2020-04-15 22:52:43 +0800

    delete not necessary libs

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-import os
-import re
-
-from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class ML:
